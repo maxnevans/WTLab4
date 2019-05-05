6cf67c7bd70af6cb17b867c9491518e5e34f3bcb
maxnevans <maxnevans@gmail.com>
2019-05-05 12:21:45 +0300

    Preloader added

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Lab 4</title>
+    <link rel="stylesheet" href="preloader.css">
 </head>
 <body>
+    <div class="preloader"></div>
     <form action="function.php" method="POST" enctype="multipart/form-data" name="search-form" id="sf">
         <input type="text" class="search-query" name="search">
         <input type="file" name="file" class="file-uploader">
