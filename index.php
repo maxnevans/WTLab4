68aaba7d87a9f910137956282b748b76c392dd5e
maxnevans <maxnevans@gmail.com>
2019-05-05 15:20:51 +0300

    Styles and labels added to the interface

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Lab 4</title>
     <link rel="stylesheet" href="preloader.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <form action="function.php" method="POST" enctype="multipart/form-data" name="search-form" id="sf">
-        <input type="text" class="search-query" name="search">
-        <input type="file" name="file" class="file-uploader">
+    <form action="function.php" method="POST" enctype="multipart/form-data" name="search-form" id="sf" class="main-form">
+        <label>Key phrase: <input type="text" class="search-query" name="search"></label>
+        <label>File: <input type="file" name="file" class="file-uploader"></label>
         <input type="submit" value="Search" name="search-go">
     </form>
 
